2c58d454e963ed0db9882c7730ba01722065df07
Jan Hartigan <janhartigan13@gmail.com>
2011-04-24 15:02:47 -0700

    adjustments to 404 page

--- application/views/page_404.php
+++ application/views/page_404.php
@@ -1,1 +1,6 @@
-<em>There doesn't appear to be any page here. Try using the navigation above.</em>
+<div id="error_404">
+	<img src="/images/Web-page-Y-U-NO-EXIST.jpg" />
+	<br/>
+	<br/>
+	<em>There doesn't appear to be any page here. Try using the navigation above.</em>
+</div>
