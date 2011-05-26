b902bdb3d5603960879938750ec19629f42e2acf
Jan Hartigan <janhartigan13@gmail.com>
2011-05-26 16:23:53 -0700

    add stackoverflow to contact page

--- application/views/contact.php
+++ application/views/contact.php
@@ -5,5 +5,6 @@
 <div id="contact_methods">
 	<a href="http://twitter.com/#!/janhartigan" class="twitter green_hover">Twitter</a>
 	<a href="https://github.com/janhartigan" class="github green_hover">GitHub</a>
+	<a href="http://stackoverflow.com/users/385950/treeface" class="stackoverflow green_hover">stackoverflow</a>
 	<a href="mailto:jan@janhartigan.com" class="email green_hover">Email</a>
 </div>
